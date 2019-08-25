58c179c67705e04a6649be1b8139d8b314fbc842
Satvik Kaul <satvik.1799@gmail.com>
2019-08-25 16:23:53 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,22 +1,8 @@
+
 class Dog
-
-  def initialize(name, breed)
-    @name=name
-    @breed = "Mutt"
-  end
-
-  def name=(name)
-    @name=name
-  end
-  def name
-    @name
-  end
-
-  def breed=(breed)
-    @breed = breed
-  end
-
-  def breed
-    @breed
-  end
+def initialize(name, breed = "Mutt")
+@name = name
+@breed = breed
 end
+end
+
